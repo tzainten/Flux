491cd5bb369fce5e7c80050fe1dff237f03e0242
tzainten <tzainten+github@gmail.com>
2026-03-20 05:29:16 -0400

    Add flux_new cmd
    Support injecting multiple mods into one target package

--- Code/Flux.cs
+++ Code/Flux.cs
@@ -1,8 +1,14 @@
 ﻿using BepInSbox;
 using BepInSbox.Core.Sbox;
+using BepInSbox.Logging;
 using HarmonyLib;
 using Sandbox;
+using Sandbox.Engine;
+using Sandbox.Services;
+using System.Collections.Concurrent;
 using System.Reflection;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace Flux;
 
@@ -11,23 +17,111 @@ public class Flux : BaseSandboxPlugin
 {
 	public static Flux Instance;
 
+	public string Root;
+
+	public string SandboxRoot;
+
+	public Dictionary<string, List<FluxProject>> Projects = new();
+
+	public Package Package;
+
+	public struct FluxProject
+	{
+		public string Package { get; set; }
+
+		[JsonIgnore]
+		public string CodePath { get; set; }
+	}
+
 	protected override void OnPluginLoad()
 	{
 		base.OnPluginLoad();
 
-		//var asm = AppDomain.CurrentDomain.GetAssemblies().First( asm => asm.GetName().Name.Contains( "Engine" ) );
-		//var pmType = asm.GetType( "Sandbox.PackageManager" );
-		//var apType = asm.GetType( "Sandbox.PackageManager+ActivePackage" );
-
-		//Logger.LogInfo( pmType );
-		//Logger.LogInfo( apType );
-
-		//var compilerType = typeof( Compiler );
-
 		Instance = this;
+
+		Root = Path.GetDirectoryName( Managed.This.Location );
+		SandboxRoot = Path.GetFullPath( Path.Combine( Root, @"..\..\" ) );
 
 		var compileGroupType = typeof( CompileGroup );
 		PatchCompileGroupConstructor( compileGroupType );
+		PatchPackageSetter();
+
+		GatherAllProjects();
+		InjectCommands();
+	}
+
+	void InjectCommands()
+	{
+		var conVarSystemType = Managed.Engine.GetType( "Sandbox.ConVarSystem" );
+		var addAssembly = conVarSystemType.GetMethod( "AddAssembly", BindingFlags.Static | BindingFlags.NonPublic );
+		addAssembly?.Invoke( null, new object[] { Managed.This, "flux", null } );
+	}
+
+	private void GatherAllProjects()
+	{
+		foreach ( var dir in Directory.GetDirectories( Root ) )
+		{
+			var fluxFile = Directory.GetFiles( dir, "*.flux" ).FirstOrDefault();
+			if ( fluxFile == null )
+				continue;
+
+			var json = Json.ParseToJsonObject( File.ReadAllText( fluxFile ) );
+
+			FluxProject project = Json.Deserialize<FluxProject>( File.ReadAllText( fluxFile ) );
+			project.CodePath = Path.Combine( dir, "Code" );
+			AddProject( project );
+		}
+	}
+
+	[ConCmd( "flux_new", Help = "<projectName> <package>" )]
+	private static void Cmd_CreateProject( string projectName, string package )
+	{
+		_ = Instance.CreateProject( projectName, package );
+	}
+
+	private async Task CreateProject( string projectName, string package )
+	{
+		var pack = await Package.FetchAsync( package, true );
+		if ( pack == null )
+			return;
+
+		var folder = Path.Combine( Root, projectName );
+		Directory.CreateDirectory( folder );
+
+		FluxProject project = new();
+		project.Package = package;
+		project.CodePath = Path.Combine( folder, "Code" );
+
+		CopyDirectory( projectName, package, Path.GetFullPath( Path.Combine( Root, @"..\flex\project_template" ) ), folder );
+
+		AddProject( project );
+	}
+
+	static void CopyDirectory( string projectName, string package, string source, string destination )
+	{
+		Directory.CreateDirectory( destination );
+
+		foreach ( var file in Directory.GetFiles( source ) )
+		{
+			var path = Path.Combine( destination, Path.GetFileName( file ).Replace( "$projectName", projectName ) );
+			File.Copy( file, path, overwrite: true );
+			var contents = File.ReadAllText( path );
+			contents = contents.Replace( "$projectName", projectName );
+			contents = contents.Replace( "$package", package );
+			contents = contents.Replace( "$sbox", Instance.SandboxRoot );
+			contents = contents.Replace( "$root", Instance.Root );
+			File.WriteAllText( path, contents );
+		}
+
+		foreach ( var dir in Directory.GetDirectories( source ) )
+			CopyDirectory( projectName, package, dir, Path.Combine( destination, Path.GetFileName( dir ) ) );
+	}
+
+	private void AddProject( FluxProject project )
+	{
+		if ( !Projects.ContainsKey( project.Package ) )
+			Projects.Add( project.Package, new() );
+		Projects[project.Package].Add( project );
 	}
 
 	private void PatchCompileGroupConstructor( Type compileGroupType )
@@ -37,28 +131,56 @@ public class Flux : BaseSandboxPlugin
 		foreach ( var ctor in ctors )
 		{
 			var postfix = new HarmonyMethod( typeof( Flux )
-				.GetMethod( nameof( CompileGroupCtorPostfix ), BindingFlags.Static | BindingFlags.NonPublic ) );
+				.GetMethod( nameof( CompileGroup_Constructor ), BindingFlags.Static | BindingFlags.NonPublic ) );
 
 			HarmonyInstance.Patch( ctor, postfix: postfix );
 		}
 	}
 
 	[HarmonyPostfix]
-	private static void CompileGroupCtorPostfix( object __instance, string name )
+	private static void CompileGroup_Constructor( object __instance, string name )
 	{
-		var compileGroup = (CompileGroup)__instance;
-		if ( name == "sandbox" )
+		if ( Instance.Package == null )
+			return;
+
+		var fullIdent = Instance.Package.FullIdent;
+		if ( Instance.Projects.ContainsKey( fullIdent ) )
 		{
-			var compiler = compileGroup.GetOrCreateCompiler( "Flux.Loader" );
+			var compileGroup = (CompileGroup)__instance;
+			foreach ( var project in Instance.Projects[fullIdent] )
+			{
+				var compiler = compileGroup.GetOrCreateCompiler( $"flux.{fullIdent}" );
 
-			compiler.GeneratedCode.AppendLine( $"global using Microsoft.AspNetCore.Components;" );
-			compiler.GeneratedCode.AppendLine( $"global using Microsoft.AspNetCore.Components.Rendering;" );
-			compiler.GeneratedCode.AppendLine( $"global using static Sandbox.Internal.GlobalGameNamespace;" );
+				compiler.GeneratedCode.AppendLine( $"global using Microsoft.AspNetCore.Components;" );
+				compiler.GeneratedCode.AppendLine( $"global using Microsoft.AspNetCore.Components.Rendering;" );
+				compiler.GeneratedCode.AppendLine( $"global using static Sandbox.Internal.GlobalGameNamespace;" );
 
-			compiler.AddReference( "package.facepunch.sandbox" );
-			compiler.AddSourcePath( @"C:\Users\tzainten\Desktop\Code" );
-			compiler.MarkForRecompile();
+				compiler.AddReference( $"package.{fullIdent}" );
+				compiler.AddSourcePath( project.CodePath );
+				compiler.MarkForRecompile();
+			}
 		}
+
+		Instance.Package = null;
+	}
+
+	private void PatchPackageSetter()
+	{
+		var activePackageType = Managed.Engine.GetType( "Sandbox.PackageManager+ActivePackage" );
+		var packageProperty = activePackageType.GetProperty( "Package",
+			BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic );
+		var packageSetter = packageProperty.SetMethod;
+
+		var postfix = new HarmonyMethod( typeof( Flux )
+			.GetMethod( nameof( PackageSetterPostfix ), BindingFlags.Static | BindingFlags.NonPublic ) );
+
+		HarmonyInstance.Patch( packageSetter, postfix: postfix );
+	}
+
+	[HarmonyPostfix]
+	private static void PackageSetterPostfix( object __instance, Package value )
+	{
+		Instance.Package = value;
 	}
 
 	protected override void OnUpdate()
---
+++ Code/Managed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Flux;
+
+internal static class Managed
+{
+	internal static Assembly This => Assembly.GetExecutingAssembly();
+	internal static Assembly Engine => AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault( asm => asm.GetName().Name.Contains( "Engine" ) );
+	internal static Assembly Access => AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault( asm => asm.GetName().Name.Contains( "Access" ) );
+}
